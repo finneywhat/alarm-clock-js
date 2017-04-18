872fce799feee1f83b9ba08bc3f377928accfc4d
Chris Finney and Cameron Jensen <cameron@>
2017-04-18 11:53:02 -0700

    able to compare input time with moment time

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -3,12 +3,11 @@ var Alarm = require('./../js/alarm-clock.js').alarmModule;
 $(function() {
   $('#alarm-clock-form').submit(function(event) {
     event.preventDefault();
-    var inputTime = $("#alarm").val();
-    console.log(inputTime);
-    var newAlarm = new Alarm(inputTime);
-    console.log(newAlarm);
-    var alarmSet = newAlarm.triggerAlarm(inputTime);
-    console.log(alarmSet);
+    var inputHour = parseInt($("#alarmHour").val());
+    var inputMinute = parseInt($("#alarmMinute").val());
+    var newAlarm = new Alarm(inputHour, inputMinute);
+
+    var alarmSet = newAlarm.triggerAlarm(inputHour, inputMinute);
 
     $("#time").text(alarmSet);
   });
--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,9 +1,20 @@
-function Alarm(time) {
-  this.time = time;
+function Alarm(inputHour, inputMinute) {
+  this.inputHour = inputHour;
+  this.inputMinute = inputMinute;
 }
+// var AlarmList = [];
+//
+//
+// Alarm.prototype.addToList = function(time) {
+//   AlarmList.push(time);
+// }
 
-Alarm.prototype.triggerAlarm = function(time) {
-  
+Alarm.prototype.triggerAlarm = function(inputHour, inputMinute) {
+  if (inputHour === moment().hour()  && inputMinute === moment().minute()) {
+    console.log("No more sleep");
+  } else {
+    console.log("Keep sleeping");
+  }
 };
 
 exports.alarmModule = Alarm;
